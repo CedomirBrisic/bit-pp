5ddfe98f521123bc548ec204277848eebbcbe3e1
BIT Student <classroom@bgit.rs>
2018-02-05 11:35:06 +0100

    Switch practices

---
+++ switch/switch2.js
@@ -0,0 +1,28 @@
+var day = '60';
+switch (day) {
+    case '1':
+        console.log('Monday');
+        break;
+    case '2':
+        console.log('Tuesday');
+        break;
+    case '3':
+        console.log('Wensday');
+    case '4':
+        console.log('Thursday')
+        break;
+    case '5':
+        console.log('Friday');
+        break;
+    case '6':
+        console.log('Saturday');
+        break;
+    case '7':
+        console.log('Sunday');
+        break;
+       
+    default:
+        console.log('Input must be a number between 1 and 7')
+
+
+}
